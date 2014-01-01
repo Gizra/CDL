e0885967f25621feeeac76d11cfafe066efbcce6
Amitai Burstein <amitai@gizra.com>
2014-01-01 18:30:30 +0200

    Theme focused nodes on homepage.

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,8 +66,8 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
-        livereload: 35729,
+        port: 8000,
+        livereload: 35730,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
