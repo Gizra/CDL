02f7992139141d875d26855b3dd56979b234835c
Carlos Mantilla <carlos@gizra.com>
2014-02-04 15:28:16 +0200

    Center text inside the circle

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function (grunt) {
     },
     watch: {
       compass: {
-        files: ['<%= yeoman.app %>/_scss/**/*.{scss,sass}'],
+        files: ['<%= yeoman.app %>/_scss/**/*.{scss,sass,css}'],
         tasks: ['compass:server', 'autoprefixer:server']
       },
       autoprefixer: {
